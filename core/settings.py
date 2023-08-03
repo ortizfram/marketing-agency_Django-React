1910825aef9ef169ab66429dc9a3e374222fa303
ortizfram <ortizfranco48@gmail.com>
2023-08-03 08:34:34 -0300

    hosts dev deploy

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
+if not DEBUG:
+    ALLOWED_HOSTS=env.list('ALLOWED_HOSTS_DEPLOY')
 
 
 # Application definition
