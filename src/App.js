43bdf3f4c13a689b21ead817196a8935794803e1
ortizfram <ortizfranco48@gmail.com>
2023-08-04 10:36:54 -0300

    react structure and packages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-gray-500 underline"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
