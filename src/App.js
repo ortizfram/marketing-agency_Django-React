ac0ce17060a225df62b813fe8649bfa006972590
ortizfram <ortizfranco48@gmail.com>
2023-08-04 11:23:54 -0300

    home display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Error404 from "containers/errors/Error404";
+import Home from "containers/pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Error Display */}
         <Route path="*" element={<Error404 />} />
+        {/* Home Display */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
