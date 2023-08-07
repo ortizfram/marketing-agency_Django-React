021bcb4ef1e7cb34c49c27c4aa5f96dc4ac3e00d
ortizfram <ortizfranco48@gmail.com>
2023-08-07 11:17:16 -0300

    navlinks active

--- src/components/navegation/Navbar.js
+++ src/components/navegation/Navbar.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import logo_sharp from "assets/img/logo.png";
 import loading_dots from "assets/img/loading-dots.gif";
 
@@ -9,50 +9,50 @@ function Navbar() {
       <div className="bg-white px-4 sm:px-6">
         <div className="-ml-4 -mt-2 hidden lg:flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
           {/* LOGO */}
-          <div className="ml-4 mt-2 flex items-center">
+          <Link to="/" className="ml-4 mt-2 flex items-center">
             <img id="logo" src={logo_sharp} width={70} height={60} alt="logo" />
             <h1 className="ml-2 font-bold h-12 text-3xl flex items-center font-righteous">
               Virtual MKT
             </h1>
-          </div>
+          </Link>
           {/* menu elements */}
           <div className="ml-4 mt-2 flex-shrink-0">
-            <Link
+            <NavLink
               to="/cases"
               className="mr-4 text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out "
             >
               Cases
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/services"
               className="mr-4 text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out "
             >
               Services
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/about"
               className="mr-4 text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out "
             >
               About Us
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/careers"
               className="mr-4 text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out "
             >
               Careers
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/blog"
               className="mr-4 text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out "
             >
               Blog
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/contact"
               className="mr-4 text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out "
             >
               Contact
-            </Link>
+            </NavLink>
             <button
               type="button"
               className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-2 text-xl font-bold text-white shadow-sm transition duration-300 ease-in-out hover:bg-black focus:outline-none focus:ring-2 "
