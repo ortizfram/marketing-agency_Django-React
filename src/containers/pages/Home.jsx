728a287e14c537cd8ca244c7d3bf1edd99a36fea
melon <ortizfranco48@gmail.com>
2023-08-07 18:37:26 -0300

    cta  logoCloud BlogList templates tailwind

--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -4,8 +4,11 @@ import Footer from "./../../components/navegation/Footer";
 import Navbar from "./../../components/navegation/Navbar";
 import UseCases from "./../../components/home/UseCases";
 import Features from "./../../components/home/Features";
+import LogoCloud from "./../../components/home/LogoCloud";
+import CTA from "./../../components/home/CTA";
 import Layout from "./../../hocs/layouts/Layout";
 import { useEffect } from "react";
+import BlogList from "../../components/home/BlogList";
 
 function Home() {
   return (
@@ -16,6 +19,9 @@ function Home() {
         <Incentives />
         <UseCases />
         <Features />
+        <CTA />
+        <LogoCloud />
+        <BlogList />
       </div>
       <Footer />
     </Layout>
